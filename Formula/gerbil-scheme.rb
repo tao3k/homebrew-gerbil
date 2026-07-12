5e3f439a2587f017c2621bea4cb8e3fa1a8dce59
guangtao <gtrunsec@hardenedlinux.org>
2026-07-12 16:51:09 -0700

    feat: fix: use Apple clang for macOS Gerbil FFI with 18.2

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -3,18 +3,9 @@ class GerbilScheme < Formula
   desc "Opinionated dialect of Scheme designed for Systems Programming"
   homepage "https://cons.io"
   url "https://github.com/mighty-gerbils/gerbil.git",
-      using: :git, revision: "c5546da0bfdc3aa17c66fe1307038bc727ed0816"
-  version "0.18.1"
+      using: :git, tag: "v0.18.2", revision: "07c8481588a8b07dbf05832687817cd398902ac0"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
-  revision 1
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git, branch: "master"
-
-  bottle do
-    root_url "https://github.com/mighty-gerbils/homebrew-gerbil/releases/download/gerbil-scheme-0.18.1_1"
-    sha256 arm64_sonoma: "882f379d132dcab6476ff116cff75912d5ce3b253634f779f2ffc8eeb2ff680e"
-    sha256 ventura:      "e1ccb020a5fa90bdeb763bf3d842a27cbae610adbe80379c2508a9161afcd89d"
-    sha256 x86_64_linux: "4b30975596af96ea8e78ff7c352ba7dd6bbac91ad9df34b3e95e83d2ae1861c5"
-  end
 
   depends_on "coreutils" => :build
   depends_on "pkg-config" => :build
@@ -22,27 +13,19 @@ class GerbilScheme < Formula
   depends_on "sqlite"
   depends_on "zlib"
   on_macos do
-    depends_on "gcc@13"
+    fails_with :gcc do
+      cause "Gambit FFI bundles built with Homebrew GCC cannot resolve macOS libSystem symbols"
+    end
   end
   on_linux do
     depends_on "gcc@13"
+    fails_with :clang do
+      cause "Gerbil requires GCC on Linux"
+    end
   end
-  fails_with :gcc do
-    version "12" # Select new gcc
-    cause "Make it easy with all the same"
-  end
-  fails_with :clang do
-    cause "Must be GCC"
-  end
-
   def install
     nproc = `nproc`.to_i - 1
-
-    if OS.mac?
-      ENV.prepend_path("PATH", "/usr/local/opt/gcc/bin")
-      ENV.prepend_path("PATH", "/opt/homebrew/opt/gcc/bin")
-      ENV["LDFLAGS"] = "-Wl,-ld_classic"
-    end
+    ENV["GERBIL_BUILD_CORES"] = nproc.to_s
     if OS.linux?
       ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/bin")
       ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/sbin")
@@ -51,11 +34,17 @@ class GerbilScheme < Formula
     ENV["GERBIL_GCC"] = ENV.cc.to_s
     ENV["CC"] = ENV.cc.to_s
     ENV["CXX"] = ENV.cxx.to_s
-    ENV["GERBIL_BUILD_CORES"] = nproc.to_s
+    ENV["LDFLAGS"] = "-Wl,-ld_classic" if OS.mac?
 
-    system "echo", ENV.cc.to_s
     system ENV.cc.to_s, "--version"
-    system "./configure", "--prefix=#{prefix}", "--enable-march="
+    system "./configure",
+      "--prefix=#{prefix}",
+      "--enable-march=",
+      "--enable-smp",
+      "--disable-single-host"
+    inreplace "src/build.sh",
+      'm="make -j ${GERBIL_BUILD_CORES:-1}" && $m bootstrap && $m from-scratch',
+      'm="${MAKE:-make}" && $m bootstrap && $m from-scratch'
     system "make", "-j#{nproc}"
     system "make", "install"
 
