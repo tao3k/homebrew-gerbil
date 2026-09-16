54d0b2f41d7d379281bf90e5cb78b6f89ce6440a
guangtao <gtrunsec@hardenedlinux.org>
2026-09-16 15:13:16 +0800

    perf: enable full SMP GCC runtime

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -9,14 +9,9 @@ class GerbilScheme < Formula
       using: :git, tag: "v0.18.2", revision: "07c8481588a8b07dbf05832687817cd398902ac0"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
 
-  revision 1
+  revision 2
 
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git, branch: "master"
-
-  bottle do
-    root_url "https://github.com/tao3k/homebrew-gerbil/releases/download/gerbil-scheme-0.18.2_1"
-    sha256 arm64_tahoe: "912a79b4096b870f53e3f6fb56b1a79515d6f3996b8e0fb5d20af4d47b0c994a"
-  end
 
   depends_on "coreutils" => :build
   depends_on "pkg-config" => :build
@@ -61,15 +56,17 @@ class GerbilScheme < Formula
            "--prefix=#{prefix}",
            "--enable-march=native",
            "--enable-smp",
+           "--enable-multiple-threaded-vms",
            "--enable-single-host=0",
            "--enable-optimized-module-limit=0",
            "--enable-c-opt=-O1",
            "--enable-c-opt-rts=yes",
+           "--enable-gcc-opts",
            "--enable-inline-jumps",
            "--enable-dynamic-clib",
            "--enable-trust-c-tco",
-           "--enable-default-runtime-options=p1,tE8,f8,-8"
-    %w[prepare gambit boot-gxi stage0 stage1 stdlib].each do |target|
+           "--enable-default-runtime-options=p100%,tE8,f8,-8"
+    %w[prepare gambit boot-gxi stage0 stage1 stdlib libgerbil].each do |target|
       target_cores = (target == "gambit") ? 1 : build_cores
       ohai "Building Gerbil phase #{target} with #{target_cores} core(s)"
       with_env("GERBIL_BUILD_FLAGS" => "-j#{target_cores}") do
@@ -94,5 +91,9 @@ class GerbilScheme < Formula
     command = "#{bin}/gerbil interactive -e " \
               "\"(for-each write '(0 1 2 3 4 5 6 7 8 9))\""
     assert_equal "0123456789", shell_output(command)
+    processor_count = shell_output(
+      "#{bin}/gxi -e '(write (##current-vm-processor-count))'",
+    ).to_i
+    assert_operator processor_count, :>, 1
   end
 end
